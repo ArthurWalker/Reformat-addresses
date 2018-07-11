6f453301f2894b25892ab966df2ea5d653523d83
ArthurWalker <phuc.phamhuu2@dcu.ie>
2018-07-11 15:49:56 +0100

    Connnecting to DB succesffully

---
+++ geocode/server/ConnectMSSQL/server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+
+
+app.get('/',function(req,res) {
+    var sql = require("mssql");
+
+    // config your database
+    var config = {
+        user:'OpenDataLogin',
+        password: 'zHbF6cK8gzkCR6yKAnMF',
+        server:'172.17.0.237',
+        database:'OPEN_DATA'
+    };
+
+    // connect to your database
+    sql.connect(config,function(err) {
+        if (err) {
+            console.log(err);}
+    
+        // create request object
+        var request = new sql.Request();
+
+        // query to the database and get the records
+        var query = 'select top 10 MPRN_Address from TD_MPRN_GUID_LINK';
+        request.query(query, function (err,result) {
+            if (err) {console.log(err);}
+            else{
+                res.send(result.recordset[5].MPRN_Address);
+                console.log(result.recordset[5].MPRN_Address);
+            }
+            sql.close();
+        });
+
+    });
+});
+
+var server = app.listen(5000,function () {
+    console.log('Server is running ...');
+});
