204083ca2b618783d845fdc98ed358929f98486b
ArthurWalker <phuc.phamhuu2@dcu.ie>
2018-07-11 12:45:26 +0100

    Fix minor erros

---
+++ geocode/src/components/ConnectDB.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class ConnectDB extends React.Component {
+  render() {
+    return (
+      <div>Connecting to DB</div>
+    );
+  }
+}
+
+
+export default ConnectDB;
