eb75e90163d3ef2fba919ecd68c33e0ecd7bfd37
ArthurWalker <phuc.phamhuu2@dcu.ie>
2018-07-27 16:24:28 +0100

    Kslas

--- geocode/server/database.js
+++ geocode/server/database.js
@@ -1,8 +1,20 @@
-var config = {
+var sql = require('mssql');
+var dbConfigOpenData = {
     user: 'OpenDataLogin',
     password: 'zHbF6cK8gzkCR6yKAnMF',
-    server: '172.17.0.237',
-    database: 'OPEN_DATA'
+    server: '172.17.0.237', //SEAIREPORTSTG01
+    database: 'OPEN_DATA',
+    requestTimeout: 600000,
+    connectionTimeout :600000,
 };
 
-module.exports=config;
+
+// Global connection pool
+var poolPromise = new sql.ConnectionPool(dbConfigOpenData).connect().then(pool => {
+    console.log(' ==> SUCCESSFUL: Connect to MSSQL');
+    return pool;
+}).catch(err =>{
+    console.log(' ==> FAIL: Database Connection Failed !',err);
+});
+
+module.exports ={sql,poolPromise};
--- geocode/server/routes/start.js
+++ geocode/server/routes/start.js
@@ -1,18 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-// config your database
-var config = require('../database');
 var main_function=require('./main_function');
 
 // req: it is the url
 // res: the function tells express what to send back to the person making the request
 
 // Home page route.
-router.get('/', function (req, res) {
+router.get('/main', function (req, res) {
     // Go to views/index.pug
     //res.render('index', { title: 'Hey', message: 'Hello there!' })
-    main_function(req, res, config);
+    main_function(req, res);
 })
 
 // About page route.
