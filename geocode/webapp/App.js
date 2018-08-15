f68eec0e38f1805fbe51369ad97f8bb2dc3b3e75
ArthurWalker <phuc.phamhuu2@dcu.ie>
2018-08-15 11:53:30 +0100

    Environment setup

---
+++ geocode/webapp/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
+import Homepage from './components/Homepage';
+
+const App = () => {
+	<div>
+		<Switch>
+		</Switch>
+	</div>;
+};
+
+export default App;
+
---
+++ geocode/webapp/components/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Homepage extends React.Component {
+
+  render() {
+    return (
+      <div>Hi</div>
+    );
+  }
+}
+
+export default Homepage;
